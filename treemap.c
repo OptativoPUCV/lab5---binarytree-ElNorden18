fc9be34448b6cde093306b808966763662e0f6c0
Replit user <>
2023-04-20 17:00:44 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -45,7 +45,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-
+  
 }
 
 TreeNode * minimum(TreeNode * x){
@@ -68,7 +68,19 @@ void eraseTreeMap(TreeMap * tree, void* key){
 }
 
 Pair * searchTreeMap(TreeMap * tree, void* key) {
-    return NULL;
+  TreeNode *current = tree->current;
+  while(current != NULL)
+    {
+      if(current->pair->key > key)
+        current = current->left;
+      else
+        current = current->right;
+      if(current == NULL)
+        return NULL;
+      if(current->pair->key == key)
+        return current->pair;
+    }
+  return NULL;
 }
 
 
